f8ac2bb8c96938ca04d014030898ebc8096c6fc8
Patrick Rasmussen <43374886+therightpathy@users.noreply.github.com>
2019-09-01 05:04:50 +0200

    First commit

--- Student.cs
+++ Student.cs
@@ -6,26 +6,89 @@ using System.Threading.Tasks;
 
 namespace UnitTestingBrushUp
 {
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
     public class Student
     {
-        public string _name;
-        public string _address;
-        public int _semester;
-        public Enum _enum;
+        private string _name;
+        private string _address;
+        private int _semester;
+        private Gender _gender;
 
-        public Student(string name, string address, int semester, Enum @enum)
+        public string Name
         {
-            _name = name;
-            _address = address;
-            _semester = semester;
-            _enum = @enum;
+            get => _name;
+            set
+            {
+                CheckName(value;)
+                _name = value;
+            }
+        }
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                CheckAddress(value;)
+                _name = value;
+            }
+        public int Semester
+        {
+            get => _semester;
+            set
+            {
+                CheckSemester(value;)
+                _semester = value;
+            }
+        }
+        public Gender Gender { get => _gender; set => _gender = value; }
+
+        public Student(string name, string address, int semester, Gender gender)
+        {
+            CheckName(Name);
+            CheckAddress(Address);
+            CheckSemester(Semester);
+            Name = name;
+            Address = address;
+            Semester = semester;
         }
 
-        public string Name()
+        private static bool CheckName(string name)
         {
-
+            if (name.Length < 2)
+            {
+                throw new ArgumentException("Name must be more than two characters");
+            }
+            return true;
         }
 
+        private static void CheckAddress(string address)
+        {
+            if(string.IsNullOrWhiteSpace(address))
+            {
+                throw new ArgumentException("Address must be filled");
+            }
+        }
 
+        private static void CheckSemester(int semester)
+        {
+            if(semester < 1)
+            {
+                throw new ArgumentException("Semester must be between 1-8");
+            }
+            if (semester > 8)
+            {
+                throw new ArgumentException("Semester must be between 1-8");
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Student({0}, {1}, {2})", Name, Address, Semester);
+        }
     }
 }
