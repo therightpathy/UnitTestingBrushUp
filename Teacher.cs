d9653407e62105dd255080ba68082f384704a329
Patrick Rasmussen <43374886+therightpathy@users.noreply.github.com>
2019-09-01 14:23:23 +0200

    Finished teacher + testing

--- Teacher.cs
+++ Teacher.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UnitTestingBrushUp
 {
-    class Teacher
+    public class Teacher
     {
         public enum Gender
         {
@@ -14,21 +14,81 @@ namespace UnitTestingBrushUp
             Female
         }
 
-        private string name;
-        private string address;
-        private double salary;
+        private string _name;
+        private string _address;
+        private double _salary;
+        private Gender _gender;
 
         public string Name
         {
-            get => name;
+            get => _name;
             set
             {
                 CheckName(value);
-                name = value;
+                _name = value;
             }
         }
 
-        public string Address { get => address; set => address = value; }
-        public double Salary { get => salary; set => salary = value; }
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                CheckAddress(value);
+                _address = value;
+            }
+        }
+
+        public double Salary
+        {
+            get => _salary;
+            set
+            {
+                CheckSalary(value);
+                _salary = value;
+            }
+        }
+
+        public Gender gender
+        {
+            get => _gender;
+            set => _gender = value;
+        }
+
+        public Teacher(string name, string address, double salary, Gender gender)
+        {
+            CheckName(name);
+            CheckAddress(address);
+            CheckSalary(salary);
+            Name = name;
+            Address = address;
+            Salary = salary;
+            _gender = gender;
+        }
+
+        private static bool CheckName(string name)
+        {
+            if(name.Length <= 2)
+            {
+                throw new ArgumentException("Name must be more than two characters");
+            }
+            return true;
+        }
+
+        private static void CheckAddress(string address)
+        {
+            if(String.IsNullOrWhiteSpace(address))
+            {
+                throw new ArgumentException("Address must be filled");
+            }
+        }
+
+        private static void CheckSalary(double salary)
+        {
+            if(salary < 0)
+            {
+                throw new ArgumentOutOfRangeException("Salary must be positive");
+            }
+        }
     }
 }
