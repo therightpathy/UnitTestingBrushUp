186055bd3c63649be1e9cb37f40e58b4bea3f62d
Patrick Rasmussen <43374886+therightpathy@users.noreply.github.com>
2019-09-01 04:39:09 +0200

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingBrushUp
+{
+    public class Student
+    {
+        public string _name;
+        public string _address;
+        public int _semester;
+        public Enum _enum;
+
+        public Student(string name, string address, int semester, Enum @enum)
+        {
+            _name = name;
+            _address = address;
+            _semester = semester;
+            _enum = @enum;
+        }
+
+        public string Name()
+        {
+
+        }
+
+
+    }
+}
